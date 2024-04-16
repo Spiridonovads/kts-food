ca622cdb5bc25f0fc58de9d0b4dcd81ce713dbb5
Divnshv <spiridonova_2804a@mail.ru>
2024-04-16 14:24:05 +0300

    init edits

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import tsconfig from "./tsconfig.json";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import tsconfig from './tsconfig.json';
 
-const SRC_PATH = path.resolve(__dirname, "src");
+const SRC_PATH = path.resolve(__dirname, 'src');
 
-const parseTsConfigPaths = (
-  paths: Record<string, string[]>
-): Record<string, string> => {
+const parseTsConfigPaths = (paths: Record<string, string[]>): Record<string, string> => {
   const webpackConfigAliases: Record<string, string> = {};
 
   Object.entries(paths).forEach(([alias, paths]) => {
-    const aliasPath = paths[0].replace(/[^a-zA-Z]/g, "");
+    const aliasPath = paths[0].replace(/[^a-zA-Z]/g, '');
 
     webpackConfigAliases[alias] = path.join(SRC_PATH, aliasPath);
   });
