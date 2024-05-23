bb4cda568f0437a4f456d5d14a3b1b15b07bf033
Divnshv <spiridonova_2804a@mail.ru>
2024-05-23 12:41:50 +0300

    icon

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter /*basename="/kts-food"*/>
+    <HashRouter>
       {' '}
       <App />
     </HashRouter>
